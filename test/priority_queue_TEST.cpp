d9961266b172a798d85eebbea994171a2611c837
[Ka3apuH] <[klient.rubin92596@gmail.com]>
2022-02-25 01:12:32 +0300

    add new benchmark

--- test/priority_queue_TEST.cpp
+++ test/priority_queue_TEST.cpp
@@ -30,4 +30,24 @@ TEST_F(Testing_main_func_priority_queue,add_element){
     }
 }
 
+TEST_F(Testing_main_func_priority_queue,add_2_element){
+
+
+    unsigned int sizeOfHeap=randomDevice()%103;
+    //чтобы не было часто равно 0.... взял простое число
+
+    for (unsigned int i = 0; i < sizeOfHeap; ++i) {
+        auto element=randomDevice();
+        test_heap.add_element(element);
+        real_heap.push(element);
+    }
+
+    for (unsigned int i = 0; i < sizeOfHeap; ++i) {
+
+        ASSERT_EQ(test_heap.max_elem(),real_heap.top());
+        test_heap.pop();
+        real_heap.pop();
+    }
+}
+
 
